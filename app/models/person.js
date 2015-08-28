a7f97c5c5ddc6c4473c0346c5d77a3d6f9ae5b62
Rory Devane <rory.devane@gmail.com>
2015-08-28 11:18:58 +0100

    Person model now uses data store

--- app/models/person.js
+++ app/models/person.js
@@ -1,6 +1,10 @@
-import Model, { attr } from "ember-data/model";
+import DS from 'ember-data';
 
-export default Model.extend({
-	firstName: attr('string'),
-	birthday: attr('date')
+export default DS.Model.extend({
+	firstName: DS.attr('string'),
+	lastName: DS.attr('string'),
+	birthday: DS.attr('date'),
+	fullName: Ember.computed('firstName', 'lastName', function() {
+		return this.get('firstName') + ' ' + this.get('lastName');
+	})
 });
